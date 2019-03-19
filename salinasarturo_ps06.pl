29da8c654873c1150db2cbed0867a83718ef64fc
Arturo Salinas <jasalina@ncsu.edu>
2019-03-19 14:23:03 -0400

    bag works bless

--- salinasarturo_ps06.pl
+++ salinasarturo_ps06.pl
@@ -3,37 +3,33 @@
 :- multifile 
 	friendof/2,
 	liked/2.
+:- discontiguous
+        friend_of/2,
+        liked/2.
 % !!!!!!!!!!!!!!!!!!!!!!!!!!!!
 % Demonstration Users
 % !!!!!!!!!!!!!!!!!!!!!!!!!!!
-liked(alex, fall).
-liked(alex, north).
 
-friendof(alex, ben). 
-friendof(alex, cathy).
-friendof(alex, dan).
-
-
-
-liked(ellie, the_godfather). 
 
 % !!!!!!!!!!!!!!!!!!!!!!!!!!!!
 % PROBLEM SET 6 
 % X is the User and Y is the movie
-% Recommend movies that are higher than a 80 but not liked by the user. 
+% Recommend movies that are higher than a 75 but not liked by the user but liked by their friends. 
 % Run with setof(X, Y^recomended_movie(<USER TO GET LIST FOR>, M),_).
 % Implement setof or bagof as another predicate to use with rec movie. 
 % !!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
 
-
+% Movies that their friends have liked but that they themselves have yet to like.
+% Make sure that the film has a minimum score of an 75.
 reco(X, Y) :-
-	(friend_of(X, Friend), 
+    (friend_of(X, Friend), 
 	liked(Friend, Y), 
-	\+ liked(X, Y)), 
-	rating(Y, R), 
-	R >= 80.
+	\+ liked(X, Y)),
+	(rating(Y, R), R >= 75).
+	
 
 recommend_movie(X, Y) :-
-%	bagof(X, Y^reco(X, Y), Y).
-	reco(X, Y).
+	bagof(_, reco(X, Y), _).
+
+	
